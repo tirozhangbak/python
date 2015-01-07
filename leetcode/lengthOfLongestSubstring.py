1f7a41cbb8d1acbe1e70f662fd1feef916e47ea1
zthero <zhangtao@sogou-inc.com>
2015-01-07 21:07:56 +0800

    -m

--- leetcode/lengthOfLongestSubstring.py
+++ leetcode/lengthOfLongestSubstring.py
@@ -2,14 +2,20 @@ class Solution:
     # @return an integer
     def lengthOfLongestSubstring(self, s):
         len = 0
+        maxLen = 0
         arr = list(s)
         for index,i in enumerate(arr):
             if i not in arr[index-len+1:index]:
-                len +=1
-                print(arr[index-len+1:index])
-        return len 
+              len +=1
+              print(arr[index-len+1:index])
+            else:
+              print(arr[index-len+1:index])
+              maxLen = max(maxLen,len)
+              len = index - (arr[index-len+1:index].index(i) + index-len+1) 
+              print(len)
+        return maxLen 
 
-str = 'wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco'
+str = 'hchzvfrkmlnozjk'
 test = Solution()
 res = test.lengthOfLongestSubstring(str)
 print(res)
